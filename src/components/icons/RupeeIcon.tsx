6fae23fdfc3c89f0004381de0a790a4793d3ae12
Manikandan Mani <boopathimanikandan1234@gmail.com>
2025-06-13 14:06:09 +0000

    Currency icon is wrong change to ₹

--- src/components/icons/RupeeIcon.tsx
+++ src/components/icons/RupeeIcon.tsx
@@ -15,7 +15,7 @@ export const RupeeIcon: React.FC<RupeeIconProps> = (props) => (
   >
     <path d="M6 3h12" />
     <path d="M6 8h12" />
-    <path d="M6 13h10c4.4182782 0 8-3.1340068 8-7v0c0 3.8659932-3.5817218 7-8 7H6" />
-    <path d="M9 13L9 21" />
+    <path d="M9 8v13" />
+    <path d="M9 13h7c2.76 0 5-2.24 5-5s-2.24-5-5-5H9" />
   </svg>
 );
